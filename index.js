714f475ec70cd139a4008f770d45df1f10d9822c
gianni28 <a.gianni2003@gmail.com>
2024-11-22 10:16:47 -0500

    se modifica logica de inicio de sesion y registro

--- index.js
+++ index.js
@@ -14,18 +14,14 @@ app.post('/api/register', async (req, res) => {
   }
 
   try {
-    const usernameSnapshot = await db.collection('usuarios').where('username', '==', username).get();
-
-    if (!usernameSnapshot.empty) {
-      return res.status(400).json({ message: 'El nombre de usuario ya está en uso' });
-    }
-
+    // Verificar si ya existe un usuario con el mismo email
     const emailSnapshot = await db.collection('usuarios').where('email', '==', email).get();
 
     if (!emailSnapshot.empty) {
       return res.status(400).json({ message: 'El correo electrónico ya está en uso' });
     }
 
+    // Registrar el usuario
     await db.collection('usuarios').add({ username, email, password });
     res.status(201).json({ message: 'Usuario registrado con éxito' });
   } catch (error) {
@@ -35,14 +31,14 @@ app.post('/api/register', async (req, res) => {
 });
 
 app.post('/api/login', async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
-  if (!username || !password) {
+  if (!email || !password) {
     return res.status(400).json({ message: 'Todos los campos son obligatorios' });
   }
 
   try {
-    const snapshot = await db.collection('usuarios').where('username', '==', username).get();
+    const snapshot = await db.collection('usuarios').where('email', '==', email).get();
 
     if (snapshot.empty) {
       return res.status(401).json({ message: 'Usuario no encontrado' });
@@ -51,7 +47,7 @@ app.post('/api/login', async (req, res) => {
     const user = snapshot.docs[0].data();
 
     if (user.password === password) {
-      return res.status(200).json({ message: 'Inicio de sesión exitoso', username: user.username });
+      return res.status(200).json({ message: 'Inicio de sesión exitoso', email: user.email });
     } else {
       return res.status(401).json({ message: 'Contraseña incorrecta' });
     }
@@ -60,6 +56,7 @@ app.post('/api/login', async (req, res) => {
     res.status(500).json({ message: 'Error interno del servidor' });
   }
 });
+
 
 // Iniciar servidor
 const PORT = process.env.PORT || 5000; // Usa el puerto de Render o 5000 como fallback
